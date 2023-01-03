de5b81d069aaf29b96cb265fb3c5ac576a276a61
syoung125 <laura120555@gmail.com>
2023-01-04 01:33:29 +0900

    feat: reset, global scss 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
 
+import "../assets/styles/index.scss";
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
