7882d9a8d7f384e0783adeaeeec34a18a769232e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-05 11:51:36 +0900

    refactor: eslint 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import classNames from "classnames/bind";
+import classNames from 'classnames/bind';
 
-import styles from "./index.module.scss";
+import styles from './index.module.scss';
 
 const cx = classNames.bind(styles);
 
 export default function Home() {
-  return <main className={cx("main")}>Home</main>;
+  return <main className={cx('main')}>Home</main>;
 }
