bfa7eca4f7f92330dbafff4f0050ba1d88db99ed
syoung125 <laura120555@gmail.com>
2023-01-07 15:05:12 +0900

    feat: Icon 컴포넌트 추가

---
+++ src/shared/components/Icon/Icon.stories.tsx
@@ -0,0 +1,35 @@
+import * as icons from '@/assets/icons';
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+
+import Icon, { IconNameType } from './Icon';
+
+export default {
+  component: Icon,
+} as Meta<typeof Icon>;
+
+export const Default: StoryObj<typeof Icon> = {
+  args: {
+    name: 'Close',
+  },
+};
+
+export const All: StoryFn<typeof Icon> = (args) => {
+  return (
+    <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {Object.keys(icons).map((name) => (
+        <li
+          key={name}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: '8px',
+          }}
+        >
+          <Icon {...args} name={name as IconNameType} />
+          <p>{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
