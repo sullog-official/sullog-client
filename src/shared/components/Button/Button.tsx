78ca1c0d93fdd8a7f820b9cbb7fef25f1ffa6fa8
syoung125 <laura120555@gmail.com>
2023-01-27 04:02:31 +0900

    feat: shared/Button 컴포넌트 추가

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,15 +1,40 @@
 import classNames from 'classnames/bind';
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
 import styles from './Button.module.scss';
 
 const cx = classNames.bind(styles);
 
-type ButtonProps = {
+type BaseButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+type ButtonType = 'primary' | 'outline';
+
+type ButtonProps = Omit<BaseButtonProps, 'type'> & {
+  /** 버튼 타입 */
+  type?: ButtonType;
+  buttonType?: BaseButtonProps['type'];
   children: React.ReactNode;
 };
 
-const Button = ({ children = '' }: ButtonProps) => {
-  return <button className={cx('button')}>{children}</button>;
+const Button = ({
+  type = 'primary',
+  buttonType = 'button',
+  children = '',
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      type={buttonType}
+      className={cx('button', type, className)}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
