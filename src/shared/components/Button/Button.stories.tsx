67694ec057040b50133fdf9fd0ddf31d6198dbf3
Ko Seoyoung <laura120555@gmail.com>
2023-01-27 20:57:43 +0900

    feat: shared/Button 컴포넌트 추가 (#8)

--- src/shared/components/Button/Button.stories.tsx
+++ src/shared/components/Button/Button.stories.tsx
@@ -3,10 +3,23 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 export default {
   component: Button,
-} as Meta<typeof Button>;
-
-export const Default: StoryObj<typeof Button> = {
   args: {
     children: '버튼',
+  },
+} as Meta<typeof Button>;
+
+export const Default: StoryObj<typeof Button> = {};
+
+export const Primary: StoryObj<typeof Button> = {
+  args: {
+    type: 'primary',
+    children: 'Primary Button',
+  },
+};
+
+export const Outline: StoryObj<typeof Button> = {
+  args: {
+    type: 'outline',
+    children: 'Outline Button',
   },
 };
