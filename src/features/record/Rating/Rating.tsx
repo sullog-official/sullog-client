c564df7830f3367e5101704b1fd9a5c37abfb9ad
syoung125 <laura120555@gmail.com>
2023-01-30 03:53:49 +0900

    feat: record/Rating 컴포넌트 추가

---
+++ src/features/record/Rating/Rating.tsx
@@ -0,0 +1,75 @@
+import classNames from 'classnames/bind';
+import { useCallback, useState } from 'react';
+import { isNil } from 'lodash-es';
+import Icon from '@/shared/components/Icon';
+
+import styles from './Rating.module.scss';
+
+const cx = classNames.bind(styles);
+
+type RatingProps = {};
+
+const MIN = 0;
+const MAX = 5;
+const STEP = 0.5;
+
+const Rating = (props: RatingProps) => {
+  const [value, setValue] = useState<number>();
+
+  const handleSliderChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(parseFloat(e.target.value));
+    },
+    []
+  );
+
+  return (
+    <div className={cx('wrapper')}>
+      <label className={cx('label')} htmlFor="points">
+        드래그해서 별점을 입력해보세요.
+      </label>
+      <div className={cx('slider-wrapper')}>
+        <div className={cx('stars')}>
+          {Array(MAX)
+            .fill(0)
+            .map((_, index) => index + 1)
+            .map((v, index) => (
+              <span key={index} className={cx('star')}>
+                <Icon
+                  name="HalfStar"
+                  size={12}
+                  // TODO: (테스트를 위한 임시코드) color prop으로 수정
+                  style={{
+                    backgroundColor:
+                      !isNil(value) && v - STEP <= value ? 'purple' : 'grey',
+                  }}
+                />
+                <Icon
+                  className={cx('half-star--flipped')}
+                  name="HalfStar"
+                  size={12}
+                  // TODO: (테스트를 위한 임시코드) color prop으로 수정
+                  style={{
+                    backgroundColor:
+                      !isNil(value) && v <= value ? 'purple' : 'grey',
+                  }}
+                />
+              </span>
+            ))}
+        </div>
+        <input
+          className={cx('slider')}
+          type="range"
+          name="points"
+          min={MIN}
+          max={MAX}
+          step={STEP}
+          value={value}
+          onChange={handleSliderChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
