5f59ef75c8885a8b2f068b4e12c07e30eba1ee72
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-29 22:39:44 +0900

    feat: onClick 이벤트 추가

--- src/shared/components/BottomNavigator/BottomNavigator.tsx
+++ src/shared/components/BottomNavigator/BottomNavigator.tsx
@@ -6,18 +6,26 @@ import styles from './BottomNavigator.module.scss';
 const cx = classNames.bind(styles);
 
 const BottomNavigator = () => {
+  const navigateToWrite = () => {};
+  const navigateToFeed = () => {};
+  const openMenuDrawer = () => {};
+
   return (
     <nav className={cx('nav')}>
-      <button className={cx('writeBtn')}>
-        <Icon name="Write" size={16} />
+      <button
+        onClick={navigateToWrite}
+        type="button"
+        className={cx('writeBtn')}
+      >
+        <Icon name="Write" size={14} />
         <p>글쓰기</p>
       </button>
       <div className={cx('container')}>
-        <button className={cx('navBtn')}>
+        <button onClick={navigateToFeed} type="button" className={cx('navBtn')}>
           <Icon name="Feed" size={28} />
           <p>둘러보기</p>
         </button>
-        <button className={cx('navBtn')}>
+        <button onClick={openMenuDrawer} type="button" className={cx('navBtn')}>
           <Icon name="Menu" size={20} />
           <p>메뉴</p>
         </button>
