32519924c9f1f0e6ceb262133dc49ad04bf0e53b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-29 22:56:18 +0900

    feat: 버튼 클릭시 스타일 변경(svg 컴포넌트 색상 변경 기능 필요)

--- src/shared/components/BottomNavigator/BottomNavigator.tsx
+++ src/shared/components/BottomNavigator/BottomNavigator.tsx
@@ -1,14 +1,25 @@
 import classNames from 'classnames/bind';
 
+import { useState } from 'react';
 import Icon from '@/shared/components/Icon';
 import styles from './BottomNavigator.module.scss';
 
 const cx = classNames.bind(styles);
 
 const BottomNavigator = () => {
+  const [isFeedBtnClicked, setIsFeedBtnClicked] = useState(false);
+  const [isMenuBtnClicked, setIsMenuBtnClicked] = useState(false);
+
   const navigateToWrite = () => {};
-  const navigateToFeed = () => {};
-  const openMenuDrawer = () => {};
+
+  const navigateToFeed = () => {
+    setIsFeedBtnClicked(true);
+    setIsMenuBtnClicked(false);
+  };
+  const openMenuDrawer = () => {
+    setIsFeedBtnClicked(false);
+    setIsMenuBtnClicked(true);
+  };
 
   return (
     <nav className={cx('nav')}>
@@ -21,11 +32,19 @@ const BottomNavigator = () => {
         <p>글쓰기</p>
       </button>
       <div className={cx('container')}>
-        <button onClick={navigateToFeed} type="button" className={cx('navBtn')}>
+        <button
+          onClick={navigateToFeed}
+          type="button"
+          className={cx('navBtn', isFeedBtnClicked && 'isFeedBtnClicked')}
+        >
           <Icon name="Feed" size={28} />
           <p>둘러보기</p>
         </button>
-        <button onClick={openMenuDrawer} type="button" className={cx('navBtn')}>
+        <button
+          onClick={openMenuDrawer}
+          type="button"
+          className={cx('navBtn', isMenuBtnClicked && 'isMenuBtnClicked')}
+        >
           <Icon name="Menu" size={20} />
           <p>메뉴</p>
         </button>
