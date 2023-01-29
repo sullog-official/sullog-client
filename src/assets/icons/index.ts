fd0552a0bdbbec597b8eb904ed66665aefb0ebf6
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-29 19:37:54 +0900

    feat: Write, Feed, Menu export

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,6 @@
 export { ReactComponent as Close } from './close.svg';
 export { ReactComponent as Plus } from './plus.svg';
 export { ReactComponent as Search } from './search.svg';
+export { ReactComponent as Feed } from './feed.svg';
+export { ReactComponent as Menu } from './menu.svg';
+export { ReactComponent as Write } from './write.svg';
