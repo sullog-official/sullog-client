00188c2b258704fa99d064014c2bd5d20b65cb38
Ko Seoyoung <laura120555@gmail.com>
2023-02-01 23:32:33 +0900

    [Feature] record Rating 컴포넌트 추가 (#16)
    * feat: HalfStar 아이콘 추가
    
    * feat: resetSliderStyles 믹스인 추가
    
    * feat: record/Rating 컴포넌트 추가
    
    * chore: dev 머지로 인한 TODO 반영
    
    * refactor: slider 초기화 스타일 mixin으로 교체
    
    * refactor: 모호한 네이밍 수정

---
+++ src/features/record/Rating/Rating.stories.tsx
@@ -0,0 +1,8 @@
+import Rating from './Rating';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  component: Rating,
+} as Meta<typeof Rating>;
+
+export const Default: StoryObj<typeof Rating> = {};
---
+++ src/features/record/Rating/Rating.tsx
@@ -0,0 +1,73 @@
+import classNames from 'classnames/bind';
+import { useCallback, useState } from 'react';
+import { isNil } from 'lodash-es';
+import Icon from '@/shared/components/Icon';
+
+import styles from './Rating.module.scss';
+
+const cx = classNames.bind(styles);
+
+type RatingProps = {};
+
+const MIN = 0;
+const MAX = 5;
+const STEP = 0.5;
+
+const Rating = (props: RatingProps) => {
+  const [value, setValue] = useState<number>();
+
+  const handleSliderChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(parseFloat(e.target.value));
+    },
+    []
+  );
+
+  return (
+    <div className={cx('wrapper')}>
+      <label className={cx('label')} htmlFor="points">
+        드래그해서 별점을 입력해보세요.
+      </label>
+      <div className={cx('slider-wrapper')}>
+        <div className={cx('stars')}>
+          {Array(MAX)
+            .fill(0)
+            .map((_, index) => index + 1)
+            .map((starValue, index) => (
+              <span key={index} className={cx('star')}>
+                <Icon
+                  name="HalfStar"
+                  size={12}
+                  color={
+                    !isNil(value) && starValue - STEP <= value
+                      ? 'purple'
+                      : 'grey200'
+                  }
+                />
+                <Icon
+                  className={cx('half-star--flipped')}
+                  name="HalfStar"
+                  size={12}
+                  color={
+                    !isNil(value) && starValue <= value ? 'purple' : 'grey200'
+                  }
+                />
+              </span>
+            ))}
+        </div>
+        <input
+          className={cx('slider')}
+          type="range"
+          name="points"
+          min={MIN}
+          max={MAX}
+          step={STEP}
+          value={value}
+          onChange={handleSliderChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
