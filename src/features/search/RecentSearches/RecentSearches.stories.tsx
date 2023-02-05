62c242ddfeb94fee789f4b621a8c58bf8f1a4c25
Jeon <gino9940@gmail.com>
2023-02-05 18:53:37 +0900

    feat: style RecentSearches

--- src/features/search/RecentSearches/RecentSearches.stories.tsx
+++ src/features/search/RecentSearches/RecentSearches.stories.tsx
@@ -3,6 +3,12 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 export default {
   component: RecentSearches,
+  args: {
+    items: [
+      { id: 1, name: '항목1' },
+      { id: 2, name: '항목' },
+    ],
+  },
 } as Meta<typeof RecentSearches>;
 
 export const Default: StoryObj<typeof RecentSearches> = {};
