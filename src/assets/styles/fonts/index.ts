1b6d65f435b3427f04eacad4e21452be52fb5da1
syoung125 <laura120555@gmail.com>
2023-02-05 20:17:55 +0900

    feat: Noto sans, MapoFlowerIsland 폰트 적용

---
+++ src/assets/styles/fonts/index.ts
@@ -0,0 +1,11 @@
+import localFont from '@next/font/local';
+
+export const mapoFlowerIsland = localFont({
+  src: [
+    {
+      path: './MapoFlowerIsland.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+  ],
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,5 +5,10 @@ import styles from './index.module.scss';
 const cx = classNames.bind(styles);
 
 export default function Home() {
-  return <main className={cx('main')}>Home</main>;
+  return (
+    <main className={cx('main')}>
+      <h1 className={cx('title')}>나의 술로그</h1>
+      <p>안녕하세요</p>
+    </main>
+  );
 }
