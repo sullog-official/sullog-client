d7bc2af47ca293acccae986f4af570b4cdb5398f
Jeon <gino9940@gmail.com>
2023-02-11 00:52:06 +0900

    feat: add Map component

---
+++ src/features/home/components/Map/Map.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames/bind';
+
+import styles from './Map.module.scss';
+
+const cx = classNames.bind(styles);
+
+type MapProps = {};
+
+const Map = (props: MapProps) => {
+  return <></>;
+};
+
+export default Map;
