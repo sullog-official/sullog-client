87a915db92fa6c3923f038e11f757e90f4d13f5d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-02-15 18:40:01 +0900

    [Feature] 리스트 페이지 (#26)
    * feat: List 페이지 생성
    
    * feat: image hostname 설정
    
    * refactor: nav -> div로 바꾸고, 백그라운드 색상 지정
    
    * feat: click 이벤트 추가
    
    * feat: selectedTab 상태 추가
    
    * refactor: 바텀네비게이션바 색상 수정
    
    * feat: 상단 네비게이션바 제외 마크업 완료
    
    * asset: back 아이콘 추가
    
    * style: margin 수정
    
    * feat: 상단 네비게이션바 구현
    
    * feat: button에 aria-label 추가
    
    * refactor: div -> nav 태그로 변경
    
    * refactor: 불필요한 css 삭제
    
    * refactor: nav 태그로 감싸지 않고 바텀네비게이터를 바로 import
    
    * refactor: AlcoholCategoryTabItems -> AlcoholCategoryTab으로 묶기
    
    * feat: AlcoholCategoryTab 컴포넌트 생성
    
    * feat: type export
    
    * feat: AlcoholCategories를 export
    
    * refactor: setSelectedTab 타입 변경
    
    * refactor: h2 -> h1으로 태그 변경
    
    * refactor: justify-content: end warning 발생 -> flex-end로 교체
    
    * refactor: preview-wrapper 높이 계산식 변수화
    
    * refactor: preview-wrapper 높이 계산식 변수화

---
+++ src/shared/components/TopNavigator/TopNavigator.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames/bind';
+
+import Icon from '@/shared/components/Icon';
+
+import styles from './TopNavigator.module.scss';
+const cx = classNames.bind(styles);
+
+type TopNavigatorProps = {
+  type: 'default' | 'personal' | 'writing';
+  title: string;
+  isCompleted?: boolean;
+};
+
+const TopNavigator = ({ type, title, isCompleted }: TopNavigatorProps) => {
+  return (
+    <div className={cx('wrapper')}>
+      <button type="button" aria-label="Back" className={cx('back-btn')}>
+        <Icon name={'Back'} size={12} />
+      </button>
+      <h1 className={cx('title', type)}>{title}</h1>
+      {type === 'writing' && (
+        <button type="button" className={cx('write-btn')}>
+          <span className={cx('write-label', isCompleted)}>완료</span>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default TopNavigator;
