e529a029648b2275f25432b93fed5d01c420da3d
Jeon <gino9940@gmail.com>
2023-02-17 01:20:09 +0900

    feat: add sldier

---
+++ src/features/home/components/Slider/Slider.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames/bind';
+
+import styles from './Slider.module.scss';
+
+const cx = classNames.bind(styles);
+
+type SliderProps = {};
+
+const Slider = (props: SliderProps) => {
+  return <></>;
+};
+
+export default Slider;
