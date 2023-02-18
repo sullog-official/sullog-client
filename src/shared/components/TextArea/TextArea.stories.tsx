eaabf2a85a063541dd19e007f3e91eec69b78c31
syoung125 <laura120555@gmail.com>
2023-02-19 00:53:41 +0900

    feat: shared TextArea 컴포넌트 추가

---
+++ src/shared/components/TextArea/TextArea.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TextArea from './TextArea';
+
+export default {
+  component: TextArea,
+} as Meta<typeof TextArea>;
+
+export const Default: StoryObj<typeof TextArea> = {};
