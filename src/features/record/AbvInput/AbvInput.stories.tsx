64b5103479b7a3d736a6eed193ccdbc306e3abf2
syoung125 <laura120555@gmail.com>
2023-02-19 23:38:09 +0900

    feat: record/AbvInput 컴포넌트 추가

---
+++ src/features/record/AbvInput/AbvInput.stories.tsx
@@ -0,0 +1,8 @@
+import AbvInput from './AbvInput';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  component: AbvInput,
+} as Meta<typeof AbvInput>;
+
+export const Default: StoryObj<typeof AbvInput> = {};
