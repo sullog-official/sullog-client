c82fdb2c3d130ad936751456f285918d0d9aa50c
Ko Seoyoung <laura120555@gmail.com>
2023-02-22 00:37:09 +0900

    [Feature] 기록하기 페이지 (#35)
    * refactor: TextField 리팩토링
    
    * feat: Rating 컴포넌트 label 속성 추가
    
    * feat: record/AbvInput 컴포넌트 추가
    
    * feat: FlavorSliderGroup 컴포넌트 label 속성 추가
    
    * feat: record/DetailFlavorInput 컴포넌트 추가
    
    * feat: TextArea 컴포넌트 수정
    
    - label 추가
    - font-size 수정
    
    * feat: 글쓰기 페이지 마크업

---
+++ src/features/record/DetailFlavorInput/DetailFlavorInput.stories.tsx
@@ -0,0 +1,8 @@
+import DetailFlavorInput from './DetailFlavorInput';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  component: DetailFlavorInput,
+} as Meta<typeof DetailFlavorInput>;
+
+export const Default: StoryObj<typeof DetailFlavorInput> = {};
