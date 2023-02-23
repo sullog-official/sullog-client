0933e967b5caf9ee11e3cdd884ced33fbd0fccfd
Jeon <gino9940@gmail.com>
2023-02-23 20:27:47 +0900

    fix: prettier error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames/bind';
 
 import Map from '@/features/home/components/Map';
+import BottomNavigator from '@/shared/components/BottomNavigator';
+import TopNavigator from '@/shared/components/TopNavigator';
 
 import styles from './index.module.scss';
 
@@ -33,6 +35,7 @@ export default function Home() {
           },
         ]}
       />
+      <BottomNavigator />
     </main>
   );
 }
