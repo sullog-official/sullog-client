be239e6076ce7ee7b030fb395a65e814fc0b230a
Jeon <gino9940@gmail.com>
2023-02-23 16:52:01 +0900

    fix: scroll on overflow

--- src/features/search/RecentSearches/RecentSearches.stories.tsx
+++ src/features/search/RecentSearches/RecentSearches.stories.tsx
@@ -9,6 +9,9 @@ export default {
     items: [
       { id: 1, name: '항목1' },
       { id: 2, name: '항목2' },
+      { id: 3, name: '항목3' },
+      { id: 4, name: '항목4' },
+      { id: 5, name: '항목5' },
     ],
   },
 } as Meta<typeof RecentSearches>;
@@ -29,13 +32,38 @@ export const Default: StoryFn<typeof RecentSearches> = (args) => {
   };
 
   return (
-    <>
+    <RecentSearches
+      items={items}
+      onDeleteItem={onDeleteItem}
+      onClickItem={onClickItem}
+      onDeleteAll={onDeleteAll}
+    />
+  );
+};
+
+export const Scroll: StoryFn<typeof RecentSearches> = (args) => {
+  const [items, setItems] = useState(args.items);
+
+  const onDeleteAll = () => {
+    setItems([]);
+  };
+
+  const onDeleteItem = (id: number) => {
+    setItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const onClickItem = (id: number) => {
+    console.log(items[id]);
+  };
+
+  return (
+    <div style={{ height: '100px' }}>
       <RecentSearches
         items={items}
         onDeleteItem={onDeleteItem}
         onClickItem={onClickItem}
         onDeleteAll={onDeleteAll}
       />
-    </>
+    </div>
   );
 };
