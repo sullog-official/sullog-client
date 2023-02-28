37057f0a61090ece1d6395efaf02af2bb3889441
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-02-28 22:18:34 +0900

    asset: 카카오, 네이버 아이콘

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -13,3 +13,5 @@ export { ReactComponent as Makgeolli } from './makgeolli.svg';
 export { ReactComponent as Etc } from './etc.svg';
 export { ReactComponent as All } from './all.svg';
 export { ReactComponent as Back } from './back.svg';
+export { ReactComponent as Kakao } from './kakao.svg';
+export { ReactComponent as Naver } from './naver.svg';
