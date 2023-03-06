0a16915cbb85e5cdccb961c0db144d3d3d25e01f
Jeon <gino9940@gmail.com>
2023-03-06 11:43:46 +0900

    feat: add Alcohol type

---
+++ src/shared/types/api/alcohol/index.ts
@@ -0,0 +1,11 @@
+type Alcohol = {
+  alcoholName: string;
+  brandName: string;
+  alcoholPercent: number;
+  productionLocation: string;
+  productionLatitude: number;
+  productionLongitude: number;
+  alcoholTag: string;
+};
+
+export type { Alcohol };
