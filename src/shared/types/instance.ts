9b346c33fd5b3dd9946a73cc34632c666b478171
Jeon <gino9940@gmail.com>
2023-03-13 15:52:06 +0900

    feat: add CustomInstance type

---
+++ src/shared/types/instance.ts
@@ -0,0 +1,9 @@
+import { AxiosInstance } from 'axios';
+
+export interface CustomInstance extends AxiosInstance {
+  get<T>(...params: Parameters<AxiosInstance['get']>): Promise<T>;
+  delete<T>(...params: Parameters<AxiosInstance['delete']>): Promise<T>;
+  post<T>(...params: Parameters<AxiosInstance['post']>): Promise<T>;
+  put<T>(...params: Parameters<AxiosInstance['put']>): Promise<T>;
+  patch<T>(...params: Parameters<AxiosInstance['patch']>): Promise<T>;
+}
