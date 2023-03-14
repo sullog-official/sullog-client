a6d27e7504bfb1a4c72b6dc22a156d5db7271f99
Jeon <gino9940@gmail.com>
2023-03-14 13:21:19 +0900

    refactor: getCookie function

--- src/shared/utils/cookie.ts
+++ src/shared/utils/cookie.ts
@@ -10,15 +10,16 @@ function setCookie(name: string, value: string, days: number) {
 }
 
 // Get the value of the cookie with the given name
-function getCookie(name: string) {
-  let nameEQ = name + '=';
-  let ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+const getCookie = (name: string) => {
+  const nameEQ = `${name}=`;
+  const cookies = document.cookie.split(';').map((cookie) => cookie.trim());
+  const matchingCookie = cookies.find((cookie) => cookie.startsWith(nameEQ));
+
+  if (matchingCookie) {
+    return matchingCookie.slice(nameEQ.length);
   }
+
   return null;
-}
+};
 
 export { setCookie, getCookie };
