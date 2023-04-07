30e6485cd9014c575f37f20d53555a8e86f7e6f7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-04-07 22:34:29 +0900

    feat: kakaoLoginCallback

--- src/shared/apis/auth/kakaoLogin.ts
+++ src/shared/apis/auth/kakaoLogin.ts
@@ -1,7 +1,4 @@
 import axios from '@/shared/configs/axios';
 
-export const kakaoLoginCallback = async (code: string) => {
-  axios.get(`/kakao?code=${code}`).then((res) => {
-    console.log(res?.headers);
-  });
-};
+export const kakaoLoginCallback = async (code: string) =>
+  await axios.get(`/kakao?code=${code}`);
