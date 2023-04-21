0d5af1e05c9448121559bb473f704a9f6c940d95
Ko Seoyoung <laura120555@gmail.com>
2023-04-21 15:11:56 +0900

    feat: record 생성 관련 api 타입, 훅 추가 (#59)

---
+++ src/shared/apis/alcohols/getAlcohol.ts
@@ -0,0 +1,19 @@
+import { createQuery } from 'react-query-kit';
+
+import { Alcohol } from '@/shared/types/alcohol';
+import { request } from '@/shared/utils/request';
+
+type Variables = Pick<Alcohol, 'alcoholId'>;
+type Response = Alcohol;
+
+const getAlcohol = ({ alcoholId }: Variables) => {
+  return request<Response>({
+    method: 'get',
+    url: `/alcohols?alcoholId=${alcoholId}`,
+  });
+};
+
+export const useGetAlcohol = createQuery<Response, Variables>({
+  primaryKey: '/alcohols/:id',
+  queryFn: ({ queryKey: [, variables] }) => getAlcohol(variables),
+});
