914c0f473469c6dc557e119f1985773f22363405
Jeon Jinho <gino9940@gmail.com>
2023-04-28 09:12:12 +0900

    Revert "[REFACTOR] SearchBar 리팩토링"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames/bind';
-import { useState } from 'react';
 
 import { mapoFlowerIsland } from '@/assets/styles/fonts';
 import Map from '@/features/home/components/Map';
@@ -11,32 +10,13 @@ import styles from './index.module.scss';
 const cx = classNames.bind(styles);
 
 export default function Home() {
-  const [searchValue, setSearchValue] = useState('');
-  const [selectedFilter, setSelectedFilter] = useState<string[]>([]);
-
-  const handleFilterClick = (filter: string) => {
-    if (selectedFilter.includes(filter)) {
-      return setSelectedFilter(
-        selectedFilter.filter((item) => item !== filter)
-      );
-    }
-    setSelectedFilter([...selectedFilter, filter]);
-  };
-
   return (
     <main className={cx('main')}>
       <div className={cx('header')}>
         <h1 className={cx('header-text')} style={mapoFlowerIsland.style}>
           나의 술로그
         </h1>
-        <SearchBar
-          placeholder="Search"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          filterItems={['소주', '과실주', '막걸리', '기타', '전체']}
-          selectedFilter={selectedFilter}
-          onFilterClick={handleFilterClick}
-        />
+        <SearchBar placeholder={'Search'} />
       </div>
       <Map
         records={[
