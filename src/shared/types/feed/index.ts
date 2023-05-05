db7035ba51af594b6e574fe2b7d8a9ec1be29036
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-05-05 14:44:31 +0900

    feat: feed 타입 선언

---
+++ src/shared/types/feed/index.ts
@@ -0,0 +1,13 @@
+export type Feed = {
+  memberId: number;
+  recordId: number;
+  description: string;
+  mainPhotoPath: string;
+  alcoholId: number;
+  alcoholName: string;
+  productionLocation: string;
+  productionLatitude: number;
+  productionLongitude: number;
+  alcoholTag: string;
+  brandName: string;
+};
