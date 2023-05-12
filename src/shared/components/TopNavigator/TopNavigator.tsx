a539280a7a4422a5f0dcb18fb064b15898fcc61b
syoung125 <laura120555@gmail.com>
2023-05-13 03:35:07 +0900

    feat: PageLayout 컴포넌트 추가 및 global.scss 수정

--- src/shared/components/TopNavigator/TopNavigator.tsx
+++ src/shared/components/TopNavigator/TopNavigator.tsx
@@ -12,6 +12,7 @@ type TopNavigatorProps = {
   onBack?: VoidFunction;
   extra?: React.ReactNode;
   highlighted?: boolean;
+  children?: React.ReactNode;
 };
 
 const TopNavigator = ({
@@ -19,27 +20,31 @@ const TopNavigator = ({
   onBack,
   extra,
   highlighted,
+  children,
 }: TopNavigatorProps) => {
   const router = useRouter();
 
   return (
-    <nav className={cx('wrapper')}>
-      <button
-        type="button"
-        aria-label="뒤로가기"
-        className={cx('back-btn')}
-        onClick={onBack ?? router.back}
-      >
-        <Icon name="Back" size={12} />
-      </button>
-      <h1
-        className={cx('title', { highlighted })}
-        style={highlighted ? mapoFlowerIsland.style : {}}
-      >
-        {title}
-      </h1>
-      {extra && <div className={cx('extra')}>{extra}</div>}
-    </nav>
+    <div className={cx('container')}>
+      <nav className={cx('wrapper')}>
+        <button
+          type="button"
+          aria-label="뒤로가기"
+          className={cx('back-btn')}
+          onClick={onBack ?? router.back}
+        >
+          <Icon name="Back" size={12} />
+        </button>
+        <h1
+          className={cx('title', { highlighted })}
+          style={highlighted ? mapoFlowerIsland.style : {}}
+        >
+          {title}
+        </h1>
+        {extra && <div className={cx('extra')}>{extra}</div>}
+      </nav>
+      {children}
+    </div>
   );
 };
 
