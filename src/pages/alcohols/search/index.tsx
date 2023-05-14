152389d6dd93e2869a3f71a66b9fd36bde4577e9
Jeon <gino9940@gmail.com>
2023-05-14 18:01:28 +0900

    feat: add debounce

--- src/pages/alcohols/search/index.tsx
+++ src/pages/alcohols/search/index.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames/bind';
+import { debounce } from 'lodash-es';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useMemo, useState } from 'react';
 
 import SearchBar from '@/features/search/components/SearchBar';
 import { useSearchAlcohol } from '@/shared/apis/alcohols/searchAlcohol';
@@ -14,11 +15,13 @@ const cx = classNames.bind(styles);
 const AlcoholSearch = () => {
   const router = useRouter();
   const [searchValue, setSearchValue] = useState('');
-  const [isEnterPressed, setIsEnterPressed] = useState(false);
   const { data, fetchNextPage, hasNextPage, isLoading } = useSearchAlcohol({
     variables: { keyword: searchValue, limit: 20, cursor: 1 },
-    enabled: isEnterPressed && searchValue.trim() !== '',
+    enabled: searchValue !== '',
+    keepPreviousData: true,
   });
+
+  console.log(searchValue);
 
   const onClickItem = (alcoholId: number) => {
     router.push(`/records/create?alcoholId=${alcoholId}`);
@@ -38,8 +41,8 @@ const AlcoholSearch = () => {
           <SearchBar
             placeholder={'마신 술 이름을 검색해주세요.'}
             value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            onKeyDown={(e) => setIsEnterPressed(e.code === 'Enter')}
+            onChange={setSearchValue}
+            useDebounce
           />
         </div>
       </TopNavigator>
