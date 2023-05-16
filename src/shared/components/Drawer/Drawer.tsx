c63bd6b938dd8ee23ceadffc41f9366f16c1d265
Ko Seoyoung <laura120555@gmail.com>
2023-05-16 22:51:13 +0900

    fix: Drawer ui 깨짐 수정 (#96)

--- src/shared/components/Drawer/Drawer.tsx
+++ src/shared/components/Drawer/Drawer.tsx
@@ -1,9 +1,13 @@
 import classNames from 'classnames/bind';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Icon from '../Icon';
 
 import styles from './Drawer.module.scss';
 
 const cx = classNames.bind(styles);
+
+const TRANSITION_SPEED = 300;
 
 interface DrawerProps {
   children: React.ReactNode;
@@ -20,18 +24,44 @@ const Drawer = ({
   percentage = 83,
   position = 'right',
 }: DrawerProps) => {
+  const [isVisible, setIsVisible] = useState<boolean>(isOpen);
+
+  useEffect(() => {
+    let timer: number;
+
+    if (isOpen) {
+      setIsVisible(true);
+    } else {
+      timer = window.setTimeout(() => {
+        setIsVisible(false);
+      }, 300);
+    }
+
+    return () => window.clearTimeout(timer);
+  }, [isOpen]);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <div
       aria-hidden={isOpen ? 'false' : 'true'}
-      className={cx('container', {
-        open: isOpen,
-      })}
+      className={cx('container', { open: isOpen })}
     >
       <div
-        className={cx('drawer', position)}
+        className={cx('drawer', position, { open: isOpen })}
         style={{ width: `${percentage}%` }}
         role="dialog"
       >
+        <button
+          className={cx('close-btn')}
+          type="button"
+          aria-label="메뉴 닫기"
+          onClick={onClose}
+        >
+          <Icon name="Close" size={16} />
+        </button>
         {children}
       </div>
       <div className={cx('backdrop')} onClick={onClose} />
