8d516a0bb731758e6eebf5a3bd1ba1480813fd1f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-05-24 23:38:15 +0900

    feat: useGetStatistics 커스텀 훅 작성

---
+++ src/shared/apis/records/getStatistics.ts
@@ -0,0 +1,16 @@
+import { createQuery } from 'react-query-kit';
+
+import { Statistics } from '@/shared/types/record/statistics';
+import { request } from '@/shared/utils/request';
+
+const getStatistics = () => {
+  return request<Statistics>({
+    method: 'get',
+    url: 'records/me/statistics',
+  });
+};
+
+export const useGetStatistics = createQuery({
+  primaryKey: '/statistics',
+  queryFn: () => getStatistics(),
+});
