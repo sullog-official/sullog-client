df80aee5022202f8589a37e07e976ae238a03574
Ko Seoyoung <laura120555@gmail.com>
2023-06-06 11:46:35 +0900

    feat: 검색 페이지 모달 레이아웃으로 수정 (#118)
    * feat: 나의 술로그 검색 페이지 모달로 변경
    
    * feat: 술 검색 페이지 모달로 변경

--- src/shared/components/TopNavigator/TopNavigator.tsx
+++ src/shared/components/TopNavigator/TopNavigator.tsx
@@ -10,31 +10,39 @@ const cx = classNames.bind(styles);
 type TopNavigatorProps = {
   title: string;
   onBack?: VoidFunction;
+  /** default: true */
+  showBackButton?: boolean;
   extra?: React.ReactNode;
   highlighted?: boolean;
   children?: React.ReactNode;
+  /** default: 'white' */
+  backgroundColor?: 'white' | 'transparent';
 };
 
 const TopNavigator = ({
   title,
   onBack,
+  showBackButton = true,
   extra,
   highlighted,
   children,
+  backgroundColor = 'white',
 }: TopNavigatorProps) => {
   const router = useRouter();
 
   return (
     <div className={cx('container')}>
-      <nav className={cx('wrapper')}>
-        <button
-          type="button"
-          aria-label="뒤로가기"
-          className={cx('back-btn')}
-          onClick={onBack ?? router.back}
-        >
-          <Icon name="Back" size={12} />
-        </button>
+      <nav className={cx('wrapper', backgroundColor)}>
+        {showBackButton && (
+          <button
+            type="button"
+            aria-label="뒤로가기"
+            className={cx('back-btn')}
+            onClick={onBack ?? router.back}
+          >
+            <Icon name="Back" size={12} />
+          </button>
+        )}
         <h1
           className={cx('title', { highlighted })}
           style={highlighted ? mapoFlowerIsland.style : {}}
